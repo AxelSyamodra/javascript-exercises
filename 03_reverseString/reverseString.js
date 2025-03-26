0c00e353b1d4d11f0a207dc8b1abb1d3ad2e00b0
Syamodra <codesyamodra@gmail.com>
2025-03-26 23:57:59 +0700

    Done 03_reverseString
    Finished with the assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const array = string.split("");
+
+    return array.reverse().join('');
 };
 
 // Do not edit below this line
