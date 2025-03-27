04fd9ba2fc129f4c06704e19555d605ff105202d
Syamodra <codesyamodra@gmail.com>
2025-03-27 21:00:22 +0700

    Done with 10_fibonacci
    Finished the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(series) {
+    let prev = 0;
+    let current = 1;
+    let fibo = 0;
 
+    for (let i = 1; i < series; i++) {
+        fibo = prev + current;
+        prev = current;
+        current = fibo;
+    }
+
+    return fibo;
 };
 
 // Do not edit below this line
