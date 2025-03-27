35ff0c3af047c2d940b42d8d7a41a9ecceec14dc
Syamodra <codesyamodra@gmail.com>
2025-03-27 14:45:11 +0700

    Done with 06_leapYears
    Finish with the exercise. Utilizing chatgpt to learn how solve the problem

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                // console.log("It's a leap year");
+                return true;
+            } else {
+                // console.log("It's not leap year");
+                return false;
+            }
+        } else {
+            // console.log("It's a leap year");
+            return true;
+        }
+    } else {
+        // console.log("It's not leap year");
+        return false;
+    }
 };
 
 // Do not edit below this line
