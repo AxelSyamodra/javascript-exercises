1ec91c8fa7ad3cce3a9b583b1f3e6e6dbccea2e0
Syamodra <codesyamodra@gmail.com>
2025-03-27 15:41:54 +0700

    Done with 08_calculator
    Finished with the exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	return numbers.reduce((accumulator, currentValue) => accumulator + currentValue , 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+	return numbers[0] - numbers[1];
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+	return numbers.reduce((accumulator, currentValue) => accumulator + currentValue , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  return numbers.reduce((accumulator, currentValue) => accumulator * currentValue , 1);
 };
 
-const power = function() {
-	
+const power = function(...numbers) {
+	return Math.pow(numbers[0], numbers[1]);
 };
 
-const factorial = function() {
-	
+const factorial = function(...numbers) {
+  let result = 1;
+	if (numbers[0] === 1 || 0) {
+    return result;
+  } else {
+        for (let i = 1; i <= numbers[0]; i++) {
+            result *= i;
+        }
+        return result;
+  }
 };
 
 // Do not edit below this line
