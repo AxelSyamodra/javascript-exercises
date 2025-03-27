804f517b8d147e56616fe87ab826c6f64e41bca3
Syamodra <codesyamodra@gmail.com>
2025-03-27 23:08:10 +0700

    Done with 12_findTheOldest
    Finished with the assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest = 0;
+    let old = 0;
+    let oldPerson;
 
+    for (person of array) {
+
+        if (person.yearOfDeath) {
+            old = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            const date = new Date();
+            old = date.getFullYear() - person.yearOfBirth;
+        }
+
+        if (old > oldest) {
+            oldest = old
+            oldPerson = person;
+        }
+    }
+
+    return oldPerson;
 };
 
 // Do not edit below this line
