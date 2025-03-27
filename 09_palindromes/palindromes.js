2d6c672f5533b6f54b8de328403077b83974f725
Syamodra <codesyamodra@gmail.com>
2025-03-27 20:43:51 +0700

    Done 09_palindromes
    Finished with the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    const originalString = words.toLowerCase();
+    const reverseString = words.toLowerCase().split("").reverse().join("");
+   
+    if (originalString === reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
